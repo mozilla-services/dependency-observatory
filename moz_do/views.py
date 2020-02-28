6fad680fb0ead14d809408562c398159b79f8f60
Mark Goodwin <mgoodwin@mozilla.com>
2020-02-28 15:07:43 +0000

    add temporary support for static files

--- moz_do/views.py
+++ moz_do/views.py
@@ -1,7 +1,38 @@
+from flask import abort, Response
+
 from moz_do import moz_do
 from markupsafe import escape
+
+from os import listdir, getcwd
+from os.path import isfile, join
+
+TYPES = {
+    '.js':'text/javascript', '.json':'application/json','.html':'text/html'
+}
 
 @moz_do.route('/package/<pkgname>')
 def show_user_profile(pkgname):
     # show the package data for that package
     return 'Package %s' % escape(pkgname)
+
+@moz_do.route('/static/<filename>')
+def serve_static_file(filename):
+    # list the names of regular files that exist in the static dir
+    print(getcwd())
+    static_dir = "static"
+    files = [f for f in listdir(static_dir) if isfile(join(static_dir, f))]
+
+    # if the requested filename exists in the list of regular filenames, serve it
+    if filename in files:
+        mimetype = "test/plain"
+
+        # Cater for some common mimetypes
+        dot_pos = filename.rfind('.')
+        if -1 != dot_pos:
+            mt = TYPES.get(filename[dot_pos:])
+            if None != mt:
+                mimetype = mt
+
+        return Response(open(join(static_dir, filename), 'r').read(), mimetype=mimetype)
+    else:
+        abort(404)
