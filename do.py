dbdbd85c3a838368fdc93056e28ae2917f49ac78
Greg Guthe <gguthe@mozilla.com>
2020-03-05 11:20:31 -0500

    do: log db init

--- do.py
+++ do.py
@@ -1,10 +1,20 @@
-from flask import Flask
 import os
+import logging
+
+from flask import Flask
+
 import moz_do.models
 from moz_do import moz_do as app
 
+log = logging.getLogger("do")
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+
 if __name__ == '__main__':
     if os.environ.get('INIT_DB', False) == '1':
+        log.info("Initializing DO DB")
         moz_do.models.init_db()
 
     host = os.environ.get('HOST','0.0.0.0')
