f4399dcaa23276108ba96630b0cea95fc7f1a277
Greg Guthe <gguthe@mozilla.com>
2020-03-05 11:16:54 -0500

    compose: create DB schema if not exists on api up

--- do.py
+++ do.py
@@ -1,10 +1,14 @@
 from flask import Flask
 import os
+import moz_do.models
 from moz_do import moz_do as app
 
 if __name__ == '__main__':
+    if os.environ.get('INIT_DB', False) == '1':
+        moz_do.models.init_db()
+
     host = os.environ.get('HOST','0.0.0.0')
     port = int(os.environ.get('PORT','8000'))
     debug = bool(os.environ.get('DEBUG','False'))
-    
+
     app.run(host=host,port=port,debug=debug)
