448a5c22dad06a0b54df651fc93fcdf568d5cd96
Greg Guthe <gguthe@mozilla.com>
2020-03-12 11:28:02 -0400

    web: use FLASK_ENV instead of DEBUG env var
    refs: https://flask.palletsprojects.com/en/1.1.x/config/#environment-and-debug-features

--- depobs/website/do.py
+++ depobs/website/do.py
@@ -19,6 +19,5 @@ if __name__ == '__main__':
 
     host = os.environ.get('HOST','0.0.0.0')
     port = int(os.environ.get('PORT','8000'))
-    debug = bool(os.environ.get('DEBUG',False))
 
-    app.run(host=host,port=port,debug=debug)
+    app.run(host=host, port=port)
