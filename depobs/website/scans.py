19fe0b20ec5a7dac1d1de408aa5d9c2ca323028a
Greg Guthe <gguthe@mozilla.com>
2020-03-13 16:02:36 -0400

    web: extract package validation logic and use exception handler

--- depobs/website/scans.py
+++ depobs/website/scans.py
@@ -2,23 +2,28 @@ import re
 
 from celery import Celery
 from flask import Blueprint, jsonify, make_response, request
+from werkzeug.exceptions import BadRequest
 
 import depobs.worker.tasks as tasks
 
 tasks_api = api = Blueprint("tasks_api", __name__)
 
 
-@api.route("/scan", methods=["POST"])
-def scan():
+@api.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return dict(description=e.description), 400
+
+
+def validate_npm_package_version_query_params():
     package_names = request.args.getlist("package_name", str)
     package_versions = request.args.getlist("package_version", str)
     package_managers = request.args.getlist("package_manager", str)
     if len(package_names) > 1:
-        return dict(error="only one package name supported"), 400
+        raise BadRequest(description="only one package name supported")
     if len(package_versions) > 1:
-        return dict(error="only zero or one package version supported"), 400
+        raise BadRequest(description="only zero or one package version supported")
     if len(package_managers) > 1:
-        return dict(error="only one package manager supported"), 400
+        raise BadRequest(description="only one package manager supported")
 
     package_name = package_names[0]
     package_version = package_versions[0] if len(package_versions) else None
@@ -28,18 +33,24 @@ def scan():
         package_name
     )
     if package_name_validation_error is not None:
-        return dict(error=validation_error), 400
+        raise BadRequest(description=validation_error)
 
     if package_version:
         package_version_validation_error = tasks.get_npm_package_version_validation_error(
             package_version
         )
         if package_version_validation_error is not None:
-            return package_version_validation_error, 400
+            raise BadRequest(description=package_version_validation_error)
 
     if package_manager != "npm":
-        return dict(error="only the package manager 'npm' supported"), 400
+        raise BadRequest(description="only the package manager 'npm' supported")
 
+    return package_name, package_version, package_manager
+
+
+@api.route("/scan", methods=["POST"])
+def scan():
+    package_name, package_version, _ = validate_npm_package_version_query_params()
     result: celery.result.AsyncResult = tasks.scan_npm_package.delay(
         package_name, package_version
     )
