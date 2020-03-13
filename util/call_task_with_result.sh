81b50f0465e86e8c5b0a1b19b1c2a6864306b28d
Greg Guthe <gguthe@mozilla.com>
2020-03-13 11:47:48 -0400

    util: score package from fixtures

--- util/call_task_with_result.sh
+++ util/call_task_with_result.sh
@@ -3,7 +3,7 @@
 # for use in the worker container; starts a worker; runs a celery task and exits
 celery -A depobs.worker.tasks worker --loglevel=info &
 WORKER_PID=$!
-celery -A depobs.worker.tasks call depobs.worker.tasks.score_package --args '["@hapi/topo", "3.6.0"]' --kwargs '{}'
+celery -A depobs.worker.tasks call depobs.worker.tasks.score_package --args '["@hapi/bounce", "2.0.0"]' --kwargs '{}'
 sleep 1
 # trigger worker shutdown after finishing the running job
 kill -TERM $WORKER_PID
