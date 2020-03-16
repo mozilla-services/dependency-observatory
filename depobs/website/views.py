795752a0ba6ceb9b68605a14c407d16bfcb3fb2b
Greg Guthe <gguthe@mozilla.com>
2020-03-16 11:13:02 -0400

    web: change API to use query params for package name and version
    refs: #66

--- depobs/website/views.py
+++ depobs/website/views.py
@@ -1,7 +1,8 @@
 from flask import abort, Response, request, send_from_directory
+from werkzeug.exceptions import BadRequest
 
 from depobs.website import models, app
-from depobs.website.scans import tasks_api
+from depobs.website.scans import tasks_api, validate_npm_package_version_query_params
 from markupsafe import escape
 
 from os import listdir, getcwd
@@ -16,9 +17,16 @@ STANDARD_HEADERS = {
 app.register_blueprint(tasks_api)
 
 
-@app.route('/package/<pkgname>/<version>')
-def show_package_by_name_and_version(pkgname, version):
-    package_report = models.get_package_report(package = pkgname, version = version)
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    return dict(description=e.description), 400
+
+
+@app.route('/packages', methods=["GET"])
+def show_package_by_name_and_version_if_available():
+    package_name, package_version, _ = validate_npm_package_version_query_params()
+
+    package_report = models.get_package_report(package = package_name, version = package_version)
     if None != package_report:
         mimetype = "application/json"
         return Response(json.dumps(package_report.json_with_dependencies()), headers=STANDARD_HEADERS, mimetype=mimetype)
@@ -26,19 +34,12 @@ def show_package_by_name_and_version(pkgname, version):
         #TODO: we probably want to return data to tell the user that a report is being generated
         abort(404)
 
-@app.route('/package/<pkgname>')
-def show_package_by_name(pkgname):
-    package_report = models.get_package_report(package = pkgname)
-    if None != package_report:
-        mimetype = "application/json"
-        return Response(json.dumps(package_report.json_with_dependencies()), headers=STANDARD_HEADERS, mimetype=mimetype)
-    else:
-        #TODO: we probably want to return data to tell the user that a report is being generated
-        abort(404)
 
-@app.route('/parents/<pkgname>/<version>')
-def get_parents_by_name_and_version(pkgname, version):
-    package_report = models.get_package_report(package = pkgname, version = version)
+@app.route('/parents', methods=["GET"])
+def get_parents_by_name_and_version():
+    package_name, package_version, _ = validate_npm_package_version_query_params()
+
+    package_report = models.get_package_report(package = package_name, version = package_version)
     if None != package_report:
         mimetype = "application/json"
         return Response(json.dumps(package_report.json_with_parents()), headers=STANDARD_HEADERS, mimetype=mimetype)
