14584135ab2486e45b82890b6a444c5200590561
Greg Guthe <gguthe@mozilla.com>
2020-04-03 11:30:45 -0400

    web: remove unused get_ functions from old scoring

--- depobs/website/models.py
+++ depobs/website/models.py
@@ -301,59 +301,6 @@ def get_child_package_ids_from_parent_package_id(links: List[PackageLink], subje
     ]
 
 
-def get_ordered_package_deps_and_reports(links: List[PackageLink], name: str, version: str) -> Tuple[List[Tuple[str, str]], List[PackageReport]]:
-    deps = []
-    incomplete = False
-
-    subject = get_most_recently_inserted_package_from_name_and_version(name, version)
-    if subject is None:
-        print(f"subject dep {name} {version} not found returning empty deps and reports")
-        return [], []
-
-    print(f"subject is {subject.name} {subject.version} {subject.id}")
-
-    dependency_ids = get_child_package_ids_from_parent_package_id(links, subject)
-    print(f"found dependency ids for {subject.name} {subject.version}: {dependency_ids}")
-    maybe_dependencies = [get_package_from_id(dependency_id) for dependency_id in dependency_ids]
-    dependencies = [dep for dep in maybe_dependencies if dep is not None]
-    reports = []
-    for dependency in dependencies:
-        print(f"dependency {dependency.id} {dependency.name} {dependency.version}")
-        report = get_package_report(dependency.name, dependency.version)
-        if None == report:
-            incomplete = True
-            deps.append((dependency.name, dependency.version))
-        else:
-            reports.append(report)
-    if incomplete:
-        print(f"dependencies for {name}, {version} incomplete, re-adding to the queue")
-        deps.append((name, version))
-    else:
-        print(f"dependencies complete for {name}, {version} adding to the graph")
-        pr = PackageReport()
-        pr.package = name
-        pr.version = version
-        pr.top_score = 0
-        pr.npmsio_score = 0
-        pr.directVulnsCritical_score = 0
-        pr.directVulnsHigh_score = 0
-        pr.directVulnsMedium_score = 0
-        pr.directVulnsLow_score = 0
-        pr.indirectVulnsCritical_score = 0
-        pr.indirectVulnsHigh_score = 0
-        pr.indirectVulnsMedium_score = 0
-        pr.indirectVulnsLow_score = 0
-        pr.authors = 0
-        pr.contributors = 0
-        pr.immediate_deps = 0
-        pr.all_deps = 0
-
-        for report in reports:
-            pr.dependencies.append(report)
-        db_session.add(pr)
-        db_session.commit()
-    return deps, reports
-
 def get_vulnerabilities_report(package: str, version: str) -> Dict:
     vulns = []
     for package_name, version, severity, url, title in get_vulnerabilities(package, version):
@@ -380,15 +327,6 @@ def get_maintainers_contributors(package: str, version: str):
 def get_npm_registry_data(package: str, version: str):
     return db_session.query(NPMRegistryEntry.published_at, NPMRegistryEntry.maintainers, NPMRegistryEntry.contributors).filter_by(package_name=package, package_version=version)
 
-def get_direct_dependencies(package: str, version: str):
-    palias = aliased(PackageVersion)
-    calias = aliased(PackageVersion)
-    return db_session.query(calias.name, calias.version
-    ).filter(PackageLink.parent_package_id==palias.id
-    ).filter(palias.name==package
-    ).filter(palias.version==version
-    ).filter(PackageLink.child_package_id==calias.id)
-
 def get_vulnerability_counts(package: str, version: str):
     return db_session.query(Advisory.package_name, PackageVersion.version, Advisory.severity,
         func.count(Advisory.severity)
@@ -402,19 +340,6 @@ def get_vulnerabilities(package: str, version: str):
     ).filter_by(package_name=package
     ).filter(PackageVersion.version==version
     ).filter(Advisory.package_name==PackageVersion.name)
-
-def get_direct_dependency_reports(package: str, version: str):
-    palias = aliased(PackageVersion)
-    calias = aliased(PackageVersion)
-    return db_session.query(calias.name, calias.version, PackageLatestReport.scoring_date, PackageLatestReport.top_score, PackageLatestReport.all_deps,
-        PackageLatestReport.directVulnsCritical_score, PackageLatestReport.directVulnsHigh_score, PackageLatestReport.directVulnsMedium_score, PackageLatestReport.directVulnsLow_score,
-        PackageLatestReport.indirectVulnsCritical_score, PackageLatestReport.indirectVulnsHigh_score, PackageLatestReport.indirectVulnsMedium_score, PackageLatestReport.indirectVulnsLow_score
-    ).filter(PackageLink.parent_package_id==palias.id
-    ).filter(palias.name==package
-    ).filter(palias.version==version
-    ).filter(PackageLink.child_package_id==calias.id
-    ).filter(PackageLatestReport.package==calias.name
-    ).filter(PackageLatestReport.version==calias.version)
 
 
 def insert_package_report_placeholder_or_update_task_id(package_name: str, package_version: str, task_id: str) -> PackageReport:
