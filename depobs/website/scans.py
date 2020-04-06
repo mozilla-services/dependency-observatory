72edc5bedf81c19229a1b0d2fbf948463f3eb807
Greg Guthe <gguthe@mozilla.com>
2020-04-06 09:46:02 -0400

    web: fix 500 error on missing package_name query param

--- depobs/website/scans.py
+++ depobs/website/scans.py
@@ -29,7 +29,7 @@ def validate_npm_package_version_query_params() -> Tuple[str, str, str]:
     package_names = request.args.getlist("package_name", str)
     package_versions = request.args.getlist("package_version", str)
     package_managers = request.args.getlist("package_manager", str)
-    if len(package_names) > 1:
+    if len(package_names) != 1:
         raise BadRequest(description="only one package name supported")
     if len(package_versions) > 1:
         raise BadRequest(description="only zero or one package version supported")
