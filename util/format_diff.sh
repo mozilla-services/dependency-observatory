445f94e74adcb17700a72696b3aafbfdec51b383
Greg Guthe <gguthe@mozilla.com>
2020-04-14 12:29:53 -0400

    util: add todo to format_diff.sh

--- util/format_diff.sh
+++ util/format_diff.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # requires a running api container
+# TODO: resolve timestamp mismatches i.e. './util/format_diff.sh | patch -f' should work
 docker-compose exec api black --config pyproject.toml -q --diff . "$@"
