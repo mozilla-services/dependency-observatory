d97f1daf453ab05522ba1c5e2c0e1217f0b1decb
Greg Guthe <gguthe@mozilla.com>
2020-04-14 12:38:18 -0400

    util: move pytest args into setup.cfg; use tmpdir for cache

--- util/run_tests.sh
+++ util/run_tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # requires a running api container
-docker-compose exec api pytest \
-	       -p no:cacheprovider \
-	       -o 'filterwarnings=ignore:"@coroutine" decorator is deprecated.*:DeprecationWarning' \
-	       "$@"
+docker-compose exec api pytest "$@"
--- util/run_tests_with_coverage.sh
+++ util/run_tests_with_coverage.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # requires a running api container
-docker-compose exec api coverage run -m pytest \
-	       -p no:cacheprovider \
-	       -o 'filterwarnings=ignore:"@coroutine" decorator is deprecated.*:DeprecationWarning' \
-	       "$@"
+docker-compose exec api coverage run -m pytest "$@"
 docker-compose exec api coverage report
 docker-compose exec api coverage html
 docker cp dependency-observatory-api:/tmp/htmlcov/ $(pwd)/htmlcov/
