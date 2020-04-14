2ee9c111dc809b10d7e5fdab08345e797abd6290
Greg Guthe <gguthe@mozilla.com>
2020-04-14 12:57:59 -0400

    ci: add unit-test job

--- util/run_tests_with_coverage.sh
+++ util/run_tests_with_coverage.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # requires a running api container
-docker-compose exec api coverage run -m pytest "$@"
-docker-compose exec api coverage report
-docker-compose exec api coverage html
-docker cp dependency-observatory-api:/tmp/htmlcov/ $(pwd)/htmlcov/
-python -m webbrowser -t htmlcov/index.html
+CI=${CI:-""}
+
+if [[ "$CI" = "" ]]; then
+    docker-compose exec api coverage run -m pytest "$@"
+    docker-compose exec api coverage report
+    docker-compose exec api coverage html
+    docker cp dependency-observatory-api:/tmp/htmlcov/ $(pwd)/htmlcov/
+    python -m webbrowser -t htmlcov/index.html
+else
+    coverage run -m pytest
+    coverage report
+    coverage html
+    cp /tmp/htmlcov/ .
+fi
