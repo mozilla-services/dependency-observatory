fc2291631a24b2572e903aab50c06d553a0a3419
Greg Guthe <gguthe@mozilla.com>
2020-04-15 23:10:48 -0400

    util: quote htmlcov output dir

--- util/run_tests_with_coverage.sh
+++ util/run_tests_with_coverage.sh
@@ -7,7 +7,7 @@ if [[ "$CI" = "" ]]; then
     docker-compose exec api coverage run -m pytest "$@"
     docker-compose exec api coverage report
     docker-compose exec api coverage html
-    docker cp dependency-observatory-api:/tmp/htmlcov/ $(pwd)/htmlcov/
+    docker cp dependency-observatory-api:/tmp/htmlcov/ "$(pwd)/htmlcov/"
     python -m webbrowser -t htmlcov/index.html
 else
     set -v
