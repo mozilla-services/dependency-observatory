bc82ea2a09aa31770834550bf0a921f6202a7d95
Greg Guthe <gguthe@mozilla.com>
2020-04-20 11:48:29 -0400

    scanner: drop pipeline.name refs

--- depobs/scanner/pipelines/run_repo_tasks.py
+++ depobs/scanner/pipelines/run_repo_tasks.py
@@ -358,7 +358,7 @@ async def build_images_for_envs(
 async def run_pipeline(
     source: Generator[Dict[str, Any], None, None], args: argparse.Namespace
 ) -> AsyncGenerator[Dict, None]:
-    log.info(f"{pipeline.name} pipeline started with args {args}")
+    log.info(f"{__name__} pipeline started with args {args}")
     task_envs = list(iter_task_envs(args))
     if args.docker_build:
         await build_images_for_envs(args, task_envs)
