62aca6956f4d0e06f3362fa79908a3de4f1af72e
Greg Guthe <gguthe@mozilla.com>
2020-04-21 17:37:36 -0400

    worker: add outer_in_graph_iter

--- depobs/worker/tasks.py
+++ depobs/worker/tasks.py
@@ -25,6 +25,7 @@ import celery.result
 from flask import current_app
 import networkx as nx
 from networkx.algorithms.dag import descendants, is_directed_acyclic_graph
+from networkx.algorithms.components import condensation
 
 from depobs.website.do import create_celery_app
 import depobs.website.models as models
@@ -334,6 +335,37 @@ def score_package(
     return pr
 
 
+def outer_in_graph_iter(g: nx.DiGraph) -> Generator[List[int], None, None]:
+    """For a directed graph with unique node IDs with type int, iterates
+    from outer / leafmost / least depended upon nodes to inner nodes
+    yielding sets of node IDs.
+
+    Properties:
+
+    * yields each node ID once
+    * successive node ID sets only depend on/point to previously visited
+    nodes or other nodes within their set
+    """
+    if len(g.nodes) == 0:
+        raise StopIteration("graph has no nodes")
+
+    # > C – The condensation graph C of G. The node labels are integers
+    # > corresponding to the index of the component in the list of strongly
+    # > connected components of G. C has a graph attribute named ‘mapping’ with
+    # > a dictionary mapping the original nodes to the nodes in C to which they
+    # > belong. Each node in C also has a node attribute ‘members’ with the set
+    # > of original nodes in G that form the SCC that the node in C represents.
+    #
+    # https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.components.condensation.html#networkx.algorithms.components.condensation
+    c = condensation(g)
+    assert is_directed_acyclic_graph(c)
+    for scc_ids in outer_in_iter(c):
+        # translate scc node ids back into G node ids
+        g_node_ids = set()
+        g_node_ids.update(*[c.nodes[scc_id]["members"] for scc_id in scc_ids])
+        yield g_node_ids
+
+
 def outer_in_iter(g: nx.DiGraph) -> Generator[List[int], None, None]:
     """
     For a DAG with unique node IDs with type int, iterates from outer
@@ -343,10 +375,10 @@ def outer_in_iter(g: nx.DiGraph) -> Generator[List[int], None, None]:
     Yields each node ID once and visits them such that successive node ID sets
     only depend on/point to previously visited nodes.
     """
-    if len(g.edges) == 0 or len(g.nodes) == 0:
-        raise Exception("graph has no edges or nodes")
-    if not is_directed_acyclic_graph(g):
-        raise Exception("graph is not a DAG")
+    if len(g.nodes) == 0:
+        raise StopIteration("graph has no nodes")
+    if len(g.nodes) > 1 and not is_directed_acyclic_graph(g):
+        raise Exception("graph has more than one node and is not a DAG")
 
     visited: AbstractSet[int] = set()
     leaf_nodes = set([node for node in g.nodes() if g.out_degree(node) == 0])
