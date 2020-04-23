61404c0e5f1d6353509053ab0d0e5fc7da600628
Greg Guthe <gguthe@mozilla.com>
2020-04-23 16:05:59 -0400

    tests: test score_package_and_children

--- tests/worker/test_scoring.py
+++ tests/worker/test_scoring.py
@@ -7,36 +7,41 @@ import pytest
 import depobs.worker.scoring as m
 
 
+# NB: later values override:
+# >>> {**{'x': 2}, 'x': 1}
+# {'x': 1}
+#
+_default_report_json = {
+    "directVulnsCritical_score": 0,
+    "directVulnsHigh_score": 0,
+    "directVulnsLow_score": 0,
+    "directVulnsMedium_score": 0,
+    "indirectVulnsCritical_score": 0,
+    "indirectVulnsHigh_score": 0,
+    "indirectVulnsLow_score": 0,
+    "indirectVulnsMedium_score": 0,
+    "id": None,
+    "task_id": None,
+    "task_status": None,
+    "authors": None,
+    "contributors": None,
+    "all_deps": 0,
+    "dependencies": [],
+    "immediate_deps": 0,
+    "npmsio_score": 0,
+    "release_date": None,
+    "status": "scanned",
+    "top_score": None,
+}
+
+
 score_package_testcases = {
     "zero_npmsio_score": (
         [],
         [],
         0,
         [],
-        {
-            "all_deps": 0,
-            "authors": None,
-            "contributors": None,
-            "dependencies": [],
-            "directVulnsCritical_score": 0,
-            "directVulnsHigh_score": 0,
-            "directVulnsLow_score": 0,
-            "directVulnsMedium_score": 0,
-            "id": None,
-            "immediate_deps": 0,
-            "indirectVulnsCritical_score": 0,
-            "indirectVulnsHigh_score": 0,
-            "indirectVulnsLow_score": 0,
-            "indirectVulnsMedium_score": 0,
-            "npmsio_score": 0,
-            "package": "foo",
-            "release_date": None,
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
-            "version": "0.0.0",
-        },
+        {**_default_report_json, "package": "foo", "version": "0.0.0",},
     ),
     "nonzero_npmsio_score": (
         [],
@@ -44,27 +49,9 @@ score_package_testcases = {
         0.53,
         [],
         {
-            "all_deps": 0,
-            "authors": None,
-            "contributors": None,
-            "dependencies": [],
-            "directVulnsCritical_score": 0,
-            "directVulnsHigh_score": 0,
-            "directVulnsLow_score": 0,
-            "directVulnsMedium_score": 0,
-            "id": None,
-            "immediate_deps": 0,
-            "indirectVulnsCritical_score": 0,
-            "indirectVulnsHigh_score": 0,
-            "indirectVulnsLow_score": 0,
-            "indirectVulnsMedium_score": 0,
+            **_default_report_json,
             "npmsio_score": 0.53,
             "package": "foo",
-            "release_date": None,
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
             "version": "0.0.0",
         },
     ),
@@ -82,27 +69,13 @@ score_package_testcases = {
         0.53,
         [],
         {
-            "all_deps": 0,
-            "authors": None,
-            "contributors": None,
-            "dependencies": [],
+            **_default_report_json,
             "directVulnsCritical_score": 1,
             "directVulnsHigh_score": 1,
             "directVulnsLow_score": 1,
             "directVulnsMedium_score": 1,
-            "id": None,
-            "immediate_deps": 0,
-            "indirectVulnsCritical_score": 0,
-            "indirectVulnsHigh_score": 0,
-            "indirectVulnsLow_score": 0,
-            "indirectVulnsMedium_score": 0,
             "npmsio_score": 0.53,
             "package": "foo",
-            "release_date": None,
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
             "version": "0.0.0",
         },
     ),
@@ -112,27 +85,11 @@ score_package_testcases = {
         0.53,
         [],
         {
-            "all_deps": 0,
+            **_default_report_json,
             "authors": 0,
             "contributors": 0,
-            "dependencies": [],
-            "directVulnsCritical_score": 0,
-            "directVulnsHigh_score": 0,
-            "directVulnsLow_score": 0,
-            "directVulnsMedium_score": 0,
-            "id": None,
-            "immediate_deps": 0,
-            "indirectVulnsCritical_score": 0,
-            "indirectVulnsHigh_score": 0,
-            "indirectVulnsLow_score": 0,
-            "indirectVulnsMedium_score": 0,
             "npmsio_score": 0.53,
             "package": "foo",
-            "release_date": None,
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
             "version": "0.0.0",
         },
     ),
@@ -148,27 +105,12 @@ score_package_testcases = {
         0.53,
         [],
         {
-            "all_deps": 0,
+            **_default_report_json,
             "authors": 1,
             "contributors": 2,
-            "dependencies": [],
-            "directVulnsCritical_score": 0,
-            "directVulnsHigh_score": 0,
-            "directVulnsLow_score": 0,
-            "directVulnsMedium_score": 0,
-            "id": None,
-            "immediate_deps": 0,
-            "indirectVulnsCritical_score": 0,
-            "indirectVulnsHigh_score": 0,
-            "indirectVulnsLow_score": 0,
-            "indirectVulnsMedium_score": 0,
             "npmsio_score": 0.53,
-            "package": "foo",
             "release_date": "release day!",
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
+            "package": "foo",
             "version": "0.0.0",
         },
     ),
@@ -215,7 +157,7 @@ score_package_testcases = {
             ),
         ],
         {
-            "all_deps": 0,
+            **_default_report_json,
             "authors": 1,
             "contributors": 2,
             "dependencies": [
@@ -250,23 +192,14 @@ score_package_testcases = {
                     indirectVulnsLow_score=1,
                 ).json_with_dependencies(depth=0),
             ],
-            "directVulnsCritical_score": 0,
-            "directVulnsHigh_score": 0,
-            "directVulnsLow_score": 0,
-            "directVulnsMedium_score": 0,
-            "id": None,
             "immediate_deps": 3,
             "indirectVulnsCritical_score": 3,
             "indirectVulnsHigh_score": 2,
             "indirectVulnsLow_score": 1,
             "indirectVulnsMedium_score": 1,
             "npmsio_score": 0.53,
-            "package": "foo",
             "release_date": "release day!",
-            "status": "scanned",
-            "task_id": None,
-            "task_status": None,
-            "top_score": None,
+            "package": "foo",
             "version": "0.0.0",
         },
     ),
@@ -316,5 +249,138 @@ def test_score_package(
     assert scored.json_with_dependencies() == expected_package_report_with_deps_json
 
 
-def test_score_package_and_children():
-    pass
+outer_in_iter_testcases = {
+    # NB: digraph node IDs need to match PackageVersion ids
+    "one_node_no_edges": (
+        [[]],
+        [[]],
+        [0.0],
+        m.nx.trivial_graph(create_using=m.nx.DiGraph),
+        [m.PackageVersion(id=0, name="test-solo-pkg", version="0.1.0")],
+        [
+            m.PackageReport(
+                package="test-solo-pkg",
+                version="0.1.0",
+                status="scanned",
+                all_deps=0,
+                directVulnsCritical_score=0,
+                directVulnsHigh_score=0,
+                directVulnsLow_score=0,
+                directVulnsMedium_score=0,
+                immediate_deps=0,
+                indirectVulnsCritical_score=0,
+                indirectVulnsHigh_score=0,
+                indirectVulnsLow_score=0,
+                indirectVulnsMedium_score=0,
+                npmsio_score=0.0,
+            ).json_with_dependencies(depth=0)
+        ],
+    ),
+    "three_node_path_graph": (
+        [[], [], []],
+        [[], [], []],
+        [0.25, 0.9, 0.34],
+        m.nx.path_graph(3, create_using=m.nx.DiGraph),
+        [
+            m.PackageVersion(id=0, name="test-root-pkg", version="0.1.0"),
+            m.PackageVersion(id=1, name="test-child-pkg", version="0.0.3"),
+            m.PackageVersion(id=2, name="test-grandchild-pkg", version="2.1.0"),
+        ],
+        [
+            {
+                **_default_report_json,
+                "npmsio_score": 0.25,
+                "package": "test-grandchild-pkg",
+                "version": "2.1.0",
+            },
+            {
+                **_default_report_json,
+                "all_deps": 1,
+                "dependencies": [
+                    {
+                        **_default_report_json,
+                        "npmsio_score": 0.25,
+                        "package": "test-grandchild-pkg",
+                        "version": "2.1.0",
+                    },
+                ],
+                "immediate_deps": 1,
+                "npmsio_score": 0.9,
+                "package": "test-child-pkg",
+                "version": "0.0.3",
+            },
+            {
+                **_default_report_json,
+                "all_deps": 2,
+                "authors": None,
+                "dependencies": [
+                    {
+                        **_default_report_json,
+                        "all_deps": 1,
+                        "immediate_deps": 1,
+                        "npmsio_score": 0.9,
+                        "package": "test-child-pkg",
+                        "version": "0.0.3",
+                    },
+                ],
+                "immediate_deps": 1,
+                "npmsio_score": 0.34,
+                "package": "test-root-pkg",
+                "version": "0.1.0",
+            },
+        ],
+    ),
+}
+
+
+@pytest.mark.parametrize(
+    "vulns, npm_registry_data, npmsio_scores, graph, package_versions, expected_package_reports_with_deps_json",
+    outer_in_iter_testcases.values(),
+    ids=outer_in_iter_testcases.keys(),
+)
+def test_score_package_and_children(
+    mocker,
+    # mocked calls
+    vulns: List[
+        Iterator[Tuple[str, str, str, int]]
+    ],  # generates: package (name), version, severity, count
+    npm_registry_data: List[
+        Iterator[Tuple[Any, List[str], List[str]]]
+    ],  # generates: published_at, maintainers, contributors
+    npmsio_scores: List[Optional[int]],
+    # args
+    graph: m.nx.DiGraph,
+    package_versions: List[m.PackageVersion],
+    expected_package_reports_with_deps_json: List[Dict[str, Any]],
+):
+    dt_mock = mocker.patch("depobs.worker.scoring.datetime")
+
+    for r in expected_package_reports_with_deps_json:
+        r["scoring_date"] = dt_mock.now()
+        for r_dep in r["dependencies"]:
+            r_dep["scoring_date"] = dt_mock.now()
+
+    npmsio_score_mock = mocker.patch(
+        "depobs.worker.scoring.get_npms_io_score",
+        **{"return_value.first.side_effect": npmsio_scores},
+    )
+    mocker.patch(
+        "depobs.worker.scoring.get_npm_registry_data", side_effect=npm_registry_data
+    )
+    mocker.patch("depobs.worker.scoring.get_vulnerability_counts", side_effect=vulns)
+
+    reports = m.score_package_and_children(graph, package_versions)
+
+    # one report per node
+    assert (
+        len(graph.nodes)
+        == len(package_versions)
+        == len(reports)
+        == len(expected_package_reports_with_deps_json)
+    )
+
+    # should have correct direct and indirect dep counts
+    for report, expected_report_json in zip(
+        reports, expected_package_reports_with_deps_json
+    ):
+        assert report.json_with_dependencies() == expected_report_json
