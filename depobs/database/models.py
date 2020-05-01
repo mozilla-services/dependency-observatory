a664060dc7b73484fe7cf246bab39acc1defdf80
Greg Guthe <gguthe@mozilla.com>
2020-05-01 15:08:43 -0400

    db: add get_letter_grade to PackageReport

--- depobs/database/models.py
+++ depobs/database/models.py
@@ -102,6 +102,19 @@ class PackageReport(TaskIDMixin, db.Model):
         backref="parents",
     )
 
+    @staticmethod
+    def get_letter_grade(score: int) -> str:
+        if score >= 80:
+            return "A"
+        elif score >= 60:
+            return "B"
+        elif score >= 40:
+            return "C"
+        elif score >= 20:
+            return "D"
+        else:
+            return "E"
+
     @property
     def report_json(self) -> Dict:
         return dict(
