43acde30e148ce73013a2f54ebb6b8f67b153675
Greg Guthe <gguthe@mozilla.com>
2020-05-01 17:47:58 -0400

    db: add get_advisories_by_package_version_id method to PackageGraph

--- depobs/database/models.py
+++ depobs/database/models.py
@@ -326,6 +326,17 @@ class PackageGraph(db.Model):
             for pv_id, score in tmp.items()
         }
 
+    def get_advisories_by_package_version_id(
+        self,
+    ) -> Dict[PackageVersionID, List["Advisory"]]:
+        # TOOD: fetch all with one DB call
+        return {
+            package_version.id: get_advisories_by_package_versions(
+                [package_version]
+            ).all()
+            for package_version in self.distinct_package_versions_by_id.values()
+        }
+
     @declared_attr
     def __table_args__(cls) -> Iterable[Index]:
         return (
@@ -1180,16 +1191,18 @@ def create_tables_and_views(app: flask.app.Flask) -> None:
 
 def get_advisories_by_package_versions(
     package_versions: List[PackageVersion],
-) -> List[Advisory]:
+) -> sqlalchemy.orm.query.Query:
     """
     Returns all advisories that directly impact the provided PackageVersion objects.
+
+    >>> from depobs.website.do import create_app
+    >>> with create_app(dict(INIT_DB=False)).app_context():
+    ...     str(get_advisories_by_package_versions([PackageVersion(id=932)]))
+    ...
+    'SELECT advisories.id AS advisories_id, advisories.language AS advisories_language, advisories.package_name AS advisories_package_name, advisories.npm_advisory_id AS advisories_npm_advisory_id, advisories.url AS advisories_url, advisories.severity AS advisories_severity, advisories.cwe AS advisories_cwe, advisories.exploitability AS advisories_exploitability, advisories.title AS advisories_title \\nFROM advisories \\nWHERE advisories.vulnerable_package_version_ids @> %(vulnerable_package_version_ids_1)s'
     """
-    return (
-        db.session.query(Advisory)
-        .filter(
-            Advisory.vulnerable_package_version_ids.contains(
-                [package_version.id for package_version in package_versions]
-            )
+    return db.session.query(Advisory).filter(
+        Advisory.vulnerable_package_version_ids.contains(
+            [package_version.id for package_version in package_versions]
         )
-        .all()
     )
