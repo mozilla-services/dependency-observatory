8fdd735ba19001f6a7afef99a595cab940ba73ec
Greg Guthe <gguthe@mozilla.com>
2020-05-07 14:30:54 -0400

    scanner: replace npm reg argparse config with typed NPMRegistryClientConfig

--- depobs/scanner/clients/npm_registry.py
+++ depobs/scanner/clients/npm_registry.py
@@ -1,18 +1,26 @@
-import argparse
 import asyncio
 import aiohttp
 import backoff
 import math
-from typing import Any, AsyncGenerator, Dict, Iterable, Optional
+from typing import Any, AsyncGenerator, Dict, Iterable, Optional, TypedDict
 import logging
 
+from depobs.scanner.clients.aiohttp_client_config import AIOHTTPClientConfig
 from depobs.scanner.pipelines.util import exc_to_str
 from depobs.util.serialize_util import grouper
 
 log = logging.getLogger(__name__)
 
 
-def aiohttp_session(args: argparse.Namespace) -> aiohttp.ClientSession:
+class NPMRegistryClientConfig(
+    AIOHTTPClientConfig, total=False
+):  # don't require keys defined below
+
+    # an npm registry access token for fetch_npm_registry_metadata. Defaults NPM_PAT env var. Should be read-only.
+    npm_auth_token: str
+
+
+def aiohttp_session(config: NPMRegistryClientConfig) -> aiohttp.ClientSession:
     # "Accept": "application/json vnd.npm.install-v1+json; q=1.0, # application/json; q=0.8, */*"
     # doesn't include author and maintainer info
 
@@ -27,15 +35,15 @@ def aiohttp_session(args: argparse.Namespace) -> aiohttp.ClientSession:
     # e.g. https://registry.npmjs.com/@hapi/bounce/2.0.8
     #
     # https://replicate.npmjs.com/ (flattened scopes) seems to be busted
-    headers = {"Accept": "application/json", "User-Agent": args.user_agent}
+    headers = {"Accept": "application/json", "User-Agent": config["user_agent"]}
     # from 'npm token create --read-only' to give us a higher rate limit
-    if args.npm_auth_token:
-        headers["Authorization"] = f"Bearer {args.npm_auth_token}"
+    if config.get("npm_auth_token", None):
+        headers["Authorization"] = f"Bearer {config['npm_auth_token']}"
 
     return aiohttp.ClientSession(
         headers=headers,
-        timeout=aiohttp.ClientTimeout(total=args.total_timeout),
-        connector=aiohttp.TCPConnector(limit=args.max_connections),
+        timeout=aiohttp.ClientTimeout(total=config["total_timeout"]),
+        connector=aiohttp.TCPConnector(limit=config["max_connections"]),
         raise_for_status=True,
     )
 
@@ -69,29 +77,31 @@ def is_not_found_exception(err: Exception) -> bool:
 
 
 async def fetch_npm_registry_metadata(
-    args: argparse.Namespace, package_names: Iterable[str], total_packages: int = None
+    config: NPMRegistryClientConfig,
+    package_names: Iterable[str],
+    total_packages: int = None,
 ) -> AsyncGenerator[Dict[str, Dict], None]:
     """
     Fetches npm registry metadata for one or more node package names
     """
     total_groups: Optional[int] = None
     if total_packages:
-        total_groups = math.ceil(total_packages / args.package_batch_size)
-    async with aiohttp_session(args) as s:
+        total_groups = math.ceil(total_packages / config["package_batch_size"])
+    async with aiohttp_session(config) as s:
         async_query_with_backoff = backoff.on_exception(
             backoff.expo,
             (aiohttp.ClientResponseError, aiohttp.ClientError, asyncio.TimeoutError),
-            max_tries=args.max_retries,
+            max_tries=config["max_retries"],
             giveup=is_not_found_exception,
             logger=log,
         )(async_query)
 
-        for i, group in enumerate(grouper(package_names, args.package_batch_size)):
+        for i, group in enumerate(grouper(package_names, config["package_batch_size"])):
             log.info(f"fetching group {i} of {total_groups}")
             try:
                 group_results = await asyncio.gather(
                     *[
-                        async_query_with_backoff(s, package_name, args.dry_run)
+                        async_query_with_backoff(s, package_name, config["dry_run"])
                         for package_name in group
                         if package_name is not None
                     ]
