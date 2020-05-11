de61e808bcb21f2c00973c864877133f2d5bdbbd
Greg Guthe <gguthe@mozilla.com>
2020-05-11 17:39:43 -0400

    util: fix adding tests to the built images for CI

--- util/run_tests_with_coverage.sh
+++ util/run_tests_with_coverage.sh
@@ -4,7 +4,14 @@ CI=${CI:-""}
 CONTAINER_NAME=${CONTAINER_NAME:-"dependency-observatory-api"}
 
 rm -rf htmlcov/
-docker exec -it "$CONTAINER_NAME" coverage run -m pytest "$@"
+
+# copy tests and tes config over since CI uses built images and doesn't mount . to app
+if [[ ! "$CI" = "" ]]; then
+    docker cp setup.cfg "$CONTAINER_NAME:/app"
+    docker cp pyproject.toml "$CONTAINER_NAME:/app"
+    docker cp tests/ "$CONTAINER_NAME:/app/tests"
+fi
+docker exec -it -e PYTHONDONTWRITEBYTECODE=1 "$CONTAINER_NAME" coverage run -m pytest "$@"
 docker exec -it "$CONTAINER_NAME" coverage report
 docker exec -it "$CONTAINER_NAME" coverage html
 docker cp dependency-observatory-api:/tmp/htmlcov/ "$(pwd)/htmlcov/"
