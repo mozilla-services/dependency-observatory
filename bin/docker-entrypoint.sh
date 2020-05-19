3a49dcf04789ec13ec43d68229fff4bd338aac55
Greg Guthe <gguthe@mozilla.com>
2020-05-19 17:43:06 -0400

    bin: add optional migrate command to entrypoint

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# takes an optional 'migrate' command then the process to exec of
+# 'web', 'worker', or 'worker-dev'
+
+DB_REVISION=${DB_REVISION:-"head"}
+if [ "$1" = 'migrate' ]; then
+    flask db upgrade "$DB_REVISION"
+    shift
+fi
+
 if [ "$1" = 'web' ]; then
     python depobs/website/do.py
 elif [ "$1" = 'worker' ]; then
