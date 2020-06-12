54b2d3dab5f2b9cb9819cb216e802f6a7b7cbbe7
Greg Guthe <gguthe@mozilla.com>
2020-06-12 15:00:19 -0400

    util: add build_and_redeploy_image.sh

---
+++ util/build_and_redeploy_image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+eval $(minikube -p minikube docker-env)
+
+# build the combined api and worker image
+./util/write_version_json.sh > depobs/version.json
+docker build -t mozilla/dependency-observatory:latest .
+
+# update deployments
+kubectl set image deployments.app/api dependency-observatory-api=mozilla/dependency-observatory:latest
+kubectl set image deployments.app/worker dependency-observatory-worker=mozilla/dependency-observatory:latest
+
+# redeploy
+kubectl rollout restart deployment api worker
+kubectl rollout status deployment api
