a131638a0695139ac8aeca3ce132b59987545499
Greg Guthe <gguthe@mozilla.com>
2020-06-23 15:16:02 -0400

    web: remove celery config

--- depobs/website/config.py
+++ depobs/website/config.py
@@ -46,43 +46,6 @@ WEB_TASK_NAMES = [
     "scan_npm_package_then_build_report_tree",
 ]
 
-# Celery config
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", None)
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", None)
-
-# Set the Celery task queue
-CELERY_ACCEPT_CONTENT = ["json"]
-CELERY_IGNORE_RESULTS = True
-CELERY_REDIRECT_STDOUTS_LEVEL = "WARNING"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TASK_SERIALIZER = "json"
-
-# report status as ‘started’ when the task is executed by a worker
-CELERY_TASK_TRACK_STARTED = True
-
-CELERY_WORKER_PREFETCH_MULTIPLIER = 1
-
-# Whether to store the task return values or not (tombstones).
-#
-# https://docs.celeryproject.org/en/stable/userguide/configuration.html#task-ignore-result
-CELERY_TASK_IGNORE_RESULT = True
-
-# If set, the worker stores all task errors in the result store even if Task.ignore_result is on.
-#
-# https://docs.celeryproject.org/en/stable/userguide/configuration.html#task-store-errors-even-if-ignored
-CELERY_TASK_STORE_ERRORS_EVEN_IF_IGNORED = True
-
-# Send task-related events so that tasks can be monitored using tools like flower. Sets the default value for the workers -E argument.
-CELERY_WORKER_SEND_TASK_EVENTS = True
-
-# If enabled, a task-sent event will be sent for every task so tasks can be tracked before they’re consumed by a worker.
-CELERY_TASK_SEND_SENT_EVENT = True
-
-# in seconds
-CELERYD_TASK_SOFT_TIME_LIMIT = 1800
-CELERYD_TASK_TIME_LIMIT = 3600
-
-
 # depobs http client config
 
 _aiohttp_args = dict(
@@ -161,7 +124,6 @@ GITHUB_CLIENT = {
         github_max_retries=12,
     ),
 }
-
 
 SCAN_NPM_TARBALL_ARGS = dict(
     language="nodejs",
