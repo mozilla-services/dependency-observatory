7faade05d3c676b1495ac81004557c92dcd05cfd
Greg Guthe <gguthe@mozilla.com>
2020-06-30 13:53:06 -0400

    util: add flask_routes script

---
+++ util/flask_routes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# requires a running api container
+CONTAINER_NAME=${CONTAINER_NAME:-$(kubectl get pods -l io.kompose.service=api -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')}
+
+kubectl cp tests/ "${CONTAINER_NAME}:/tmp/"
+kubectl exec -it "$CONTAINER_NAME" -- flask routes --all-methods -s rule
