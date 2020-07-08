534000f0658528e52628c9c522a9f8b3d9d548de
Greg Guthe <gguthe@mozilla.com>
2020-07-08 18:46:58 -0400

    web: improve error messages

--- depobs/website/static/do.js
+++ depobs/website/static/do.js
@@ -14,7 +14,13 @@ async function startJob(args) {
     body: JSON.stringify(body),
   }).catch((err) => {
     console.error(`error POSTing to ${jobURI}: ${err}`);
+    throw err;
   });
+  if (response.status !== 200) {
+    let err = new Error(`${response.status} from ${jobURI}`);
+    err.response = response;
+    throw err;
+  }
   console.log("start job response: ", response);
   let responseJSON = await response.json();
 
@@ -37,14 +43,36 @@ async function checkReportExists(formData) {
 const formEl = document.getElementById("search-form");
 const formFieldsetEls = formEl.querySelectorAll("fieldset");
 
-function updateScanError(err, reportURI) {
-  const scanErrorEl = document.getElementById("scan-error");
+function updateSearchError(err, errContextMessage) {
+  // takes an Error with an optional .response property set and
+  // displays errContextMessage
+  const errorEl = document.getElementById("search-error");
+  const errorContextMessageEl = document.getElementById("error-context");
+  const errorReqIDEl = document.getElementById("error-request-id");
+  const errorIssueLinkEl = document.querySelector(".error-issue-link");
 
-  if (err) {
-    console.error(`error fetching HEAD ${reportURI}: ${err}`);
-    scanErrorEl.classList.remove("d-none");
+  if (errContextMessage) {
+    errorContextMessageEl.textContent = errContextMessage;
   } else {
-    scanErrorEl.classList.add("d-none");
+    errorContextMessageEl.textContent = "";
+  }
+  if (err) {
+    errorEl.classList.remove("d-none");
+  } else {
+    errorEl.classList.add("d-none");
+  }
+  if (
+    err &&
+    err.response &&
+    err.response.headers &&
+    err.response.headers.get("x-request-id")
+  ) {
+    let requestID = err.response.headers.get("x-request-id");
+    errorReqIDEl.textContent = requestID;
+    errorIssueLinkEl.href = `https://github.com/mozilla-services/dependency-observatory/issues/new?title=error making request&body=request id: ${requestID}`;
+  } else {
+    errorReqIDEl.textContent = "";
+    errorIssueLinkEl.href = `https://github.com/mozilla-services/dependency-observatory/issues/new?title=error making request&body=request id: replaceme`;
   }
 }
 
@@ -83,7 +111,7 @@ function redirectToJobLogs(jobName) {
 function onSubmit(event) {
   console.debug(`form submitted! timestamp: ${event.timeStamp}`);
   event.preventDefault();
-  updateScanError(null); // clear errors
+  updateSearchError(null); // clear error display
 
   let formData = new FormData(formEl);
   let formDataObj = Object.fromEntries(formData);
@@ -91,22 +119,44 @@ function onSubmit(event) {
 
   if (formDataObj.force_rescan === "on") {
     console.debug("skipping report check since rescan requested");
-    scanAndScorePackage(formDataObj).then(redirectToJobLogs);
+    scanAndScorePackage(formDataObj)
+      .then(redirectToJobLogs)
+      .catch((err) => {
+        console.error(`error starting rescan: ${err}`);
+        updateSearchError(err, "rescanning a package");
+      });
   } else {
     updateSearchForm(true); // disable the search form
-    checkReportExists(formData).then((response) => {
-      updateSearchForm(false); // enable the search form
-      if (response.status === 200) {
-        // redirect to report if it exists
-        console.debug(`report exists redirecting to ${response.url}`);
-        window.location.assign(response.url);
-      } else if (response.status !== 404) {
-        // something unexpected display an error for non-404 errors
-        updateScanError(response, response.url);
-      } else {
-        scanAndScorePackage(formDataObj).then(redirectToJobLogs);
-      }
-    });
+    checkReportExists(formData)
+      .catch((err) => {
+        updateSearchForm(false); // enable the search form
+        console.error(`error checking report exists: ${err}`);
+        updateSearchError(err, "checking a package report exists");
+      })
+      .then((response) => {
+        updateSearchForm(false); // enable the search form
+
+        if (response.status === 200) {
+          // redirect to report if it exists
+          console.debug(`report exists redirecting to ${response.url}`);
+          window.location.assign(response.url);
+          // } else if (response.status === 422) {
+          //   // TODO: display the error message for malformed requests
+          //   // updateSearchError(true, "checking a package report exists", response.headers.get("x-request-id"));
+        } else if (response.status !== 404) {
+          // something unexpected display an error for non-404 errors
+          let err = new Error();
+          err.response = response;
+          updateSearchError(err, "checking a package report exists");
+        } else {
+          scanAndScorePackage(formDataObj)
+            .then(redirectToJobLogs)
+            .catch((err) => {
+              console.error(`error starting scan: ${err}`);
+              updateSearchError(err, "scanning a package");
+            });
+        }
+      });
   }
 }
 
