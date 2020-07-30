d3ae8d104f37c017a3307ae84528282d3fcdaf41
Greg Guthe <gguthe@mozilla.com>
2020-07-30 15:02:27 -0400

    worker: add graph_id to PackageReport

--- depobs/worker/scoring.py
+++ depobs/worker/scoring.py
@@ -412,6 +412,9 @@ def score_package_graph(
             reports_by_package_version_id[dep_node_id] for dep_node_id in direct_dep_ids
         )
 
+    for report in reports_by_package_version_id.values():
+        report.graph_id = db_graph.id
+
     return reports_by_package_version_id
 
 
