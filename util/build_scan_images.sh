e17120ce9a82a0c2c0fd7445cc7362bb1c3a0081
Greg Guthe <gguthe@mozilla.com>
2020-08-03 15:17:15 -0400

    util: disable building node-10 and rust-1 scan images

--- util/build_scan_images.sh
+++ util/build_scan_images.sh
@@ -6,5 +6,5 @@ eval $(minikube -p minikube docker-env)
 
 # build scan images
 docker build --build-arg=BASE_NAME=node --build-arg=BASE_VERSION=12-buster-slim -t mozilla/dependency-observatory:node-12 ./scan_envs
-docker build --build-arg=BASE_NAME=node --build-arg=BASE_VERSION=10-buster-slim -t mozilla/dependency-observatory:node-10 ./scan_envs
-docker build --build-arg=BASE_NAME=rust --build-arg=BASE_VERSION=1-slim-buster -t mozilla/dependency-observatory:rust-1 ./scan_envs
+# docker build --build-arg=BASE_NAME=node --build-arg=BASE_VERSION=10-buster-slim -t mozilla/dependency-observatory:node-10 ./scan_envs
+# docker build --build-arg=BASE_NAME=rust --build-arg=BASE_VERSION=1-slim-buster -t mozilla/dependency-observatory:rust-1 ./scan_envs
