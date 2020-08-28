e8bdf755392717a8258f5bd588e9c59896e60cb0
Greg Guthe <gguthe@mozilla.com>
2020-08-28 10:24:10 -0400

    db: rm PackageReportColumnsMixin and mixins module

--- depobs/database/models.py
+++ depobs/database/models.py
@@ -46,7 +46,6 @@ from sqlalchemy.types import DateTime
 from sqlalchemy.schema import Table
 from sqlalchemy import func
 
-from depobs.database.mixins import PackageReportColumnsMixin
 from depobs.database.schemas import PackageReportSchema
 from depobs.website.schemas import JobParamsSchema
 
@@ -95,10 +94,30 @@ class Dependency(db.Model):
     used_by_id = Column(Integer, ForeignKey("reports.id"), primary_key=True)
 
 
-class PackageReport(PackageReportColumnsMixin, db.Model):
+class PackageReport(db.Model):
     __tablename__ = "reports"
 
     id = Column("id", Integer, primary_key=True)
+    package = Column(String(200))
+    version = Column(String(200))
+    release_date = Column(DateTime)
+    scoring_date = Column(DateTime)
+    top_score = Column(Integer)
+    npmsio_score = Column(Float)
+    npmsio_scored_package_version = Column(String)
+    directVulnsCritical_score = Column(Integer)
+    directVulnsHigh_score = Column(Integer)
+    directVulnsMedium_score = Column(Integer)
+    directVulnsLow_score = Column(Integer)
+    indirectVulnsCritical_score = Column(Integer)
+    indirectVulnsHigh_score = Column(Integer)
+    indirectVulnsMedium_score = Column(Integer)
+    indirectVulnsLow_score = Column(Integer)
+    authors = Column(Integer)
+    contributors = Column(Integer)
+    immediate_deps = Column(Integer)
+    all_deps = Column(Integer)
+    graph_id = Column(Integer, nullable=True)
 
     @staticmethod
     def score_vulns(
