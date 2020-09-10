58bcf515a5dbfd56472ea5f6a353fbe88f4a2610
Greg Guthe <gguthe@mozilla.com>
2020-09-10 16:29:37 -0400

    drop job backoff limit to 1

--- depobs/website/config.py
+++ depobs/website/config.py
@@ -85,7 +85,7 @@ SCAN_NPM_TARBALL_ARGS: Dict[
         str, int, None, Dict[str, Union[str, None]], List[Union[str, Dict[str, str]]]
     ],
 ] = dict(
-    backoff_limit=4,
+    backoff_limit=1,
     context_name=os.environ.get("UNTRUSTED_JOB_CONTEXT", None),
     namespace=os.environ.get("UNTRUSTED_JOB_NAMESPACE", "default"),
     language="nodejs",
@@ -107,7 +107,7 @@ SCAN_NPM_DEP_FILES_ARGS: Dict[
         str, int, None, Dict[str, Union[str, None]], List[Union[str, Dict[str, str]]]
     ],
 ] = dict(
-    backoff_limit=4,
+    backoff_limit=1,
     context_name=os.environ.get("UNTRUSTED_JOB_CONTEXT", None),
     namespace=os.environ.get("UNTRUSTED_JOB_NAMESPACE", "default"),
     language="nodejs",
