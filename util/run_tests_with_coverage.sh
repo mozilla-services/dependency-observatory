19aadb647d149eaa4cacdcd86a419de894c269ea
Greg Guthe <gguthe@mozilla.com>
2020-09-23 16:41:58 -0400

    convert pytest config to pyproject.toml

--- util/run_tests_with_coverage.sh
+++ util/run_tests_with_coverage.sh
@@ -5,7 +5,7 @@ rm -rf htmlcov/
 
 CONTAINER_NAME=${CONTAINER_NAME:-$(kubectl get pods -l io.kompose.service=api -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')}
 
-kubectl exec -it "$CONTAINER_NAME" -- coverage run -m pytest -c /app/setup.cfg /app/tests /app/depobs/ /app/depobs/database/models.py "$@"
+kubectl exec -it "$CONTAINER_NAME" -- coverage run -m pytest -c /app/pyproject.toml /app/tests /app/depobs/ /app/depobs/database/models.py "$@"
 kubectl exec -it "$CONTAINER_NAME" -- coverage report
 kubectl exec -it "$CONTAINER_NAME" -- coverage html
 kubectl cp "${CONTAINER_NAME}:/tmp/htmlcov" "$(pwd)/htmlcov/"
