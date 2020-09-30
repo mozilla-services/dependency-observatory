fc8a3d90b3317e2314a2b7342998a18b01cce2d7
Greg Guthe <gguthe@mozilla.com>
2020-09-30 17:54:38 -0400

    util: remove duplicate db models doctest files

--- util/run_tests.sh
+++ util/run_tests.sh
@@ -3,4 +3,4 @@
 # requires a running api container
 CONTAINER_NAME=${CONTAINER_NAME:-$(kubectl get pods -l io.kompose.service=api -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')}
 
-kubectl exec -it "$CONTAINER_NAME" -- pytest -c /app/pyproject.toml /app/tests /app/depobs/ /app/depobs/database/models.py "$@"
+kubectl exec -it "$CONTAINER_NAME" -- pytest -c /app/pyproject.toml /app/tests /app/depobs/ "$@"
